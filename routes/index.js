b6ff389e2f21a84b2338bd6fcf88fa1960090d7d
Eman Jašarević <emanj@comtrade.com>
2018-01-07 12:52:01 +0100

    Remove unused

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function (req, res, next) {
-    res.render('index', {title: 'Express'});
-});
-
 router.post('/webhook', (req, res) => {
 
     let body = req.body;
